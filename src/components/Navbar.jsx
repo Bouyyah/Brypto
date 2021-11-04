00b153675139042ae55f2398f62a46d36638a51a
bouyyah <hamameayyoub@gmail.com>
2021-11-04 22:47:29 +0100

    Router & navbar 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MennuOutlined} from '@ant-design/icons';
 
-function navBar() {
+import icon from '../images/Brypto.png';
+
+function Navbar() {
     return (
-        <div>
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large"/>
+                <Typography.Title level={2} className="logo" >
+                    <Link to="/">Brypto</Link>
+                </Typography.Title>
+                {/* <Button className="menu-control-container"></Button> */}
+            </div>
             
         </div>
     )
 }
 
-export default navBar
+export default Navbar;
