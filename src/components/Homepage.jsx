d5a0a840ef0abb72678efb3b294b1e130f51a687
bouyyah <hamameayyoub@gmail.com>
2021-11-08 01:46:15 +0100

    coins endPoint working

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,13 +3,20 @@ import millify from 'millify';
 import {Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
 const { Title } = Typography;
 
 function Homepage() {
+    const { data, isFetching } = useGetCryptosQuery();
+
+    console.log(data);
+
     return (
         <>
         <Title level={2} className="heading">
             Global Crypto Stats
+            
         </Title>
         <Row>
             <Col span={12} ><Statistic title="Total cryptocurrencies" value="5" /></Col>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': '45a4b6d1fbmsh31b048dfc5744cbp19bb52jsn6d633361274c'
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/stats';
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
 
@@ -14,7 +14,11 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/exchanges')
+            query: () => createRequest('/coins')
         })
     })
 });
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
