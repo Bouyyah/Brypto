1a2d4389f95174b586f42e6166e70515ab4f4bdb
bouyyah <hamameayyoub@gmail.com>
2021-11-10 20:39:23 +0100

    Implementing News Api and news component

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -4,7 +4,7 @@ const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': '45a4b6d1fbmsh31b048dfc5744cbp19bb52jsn6d633361274c',
     'x-access-token':'i-have-to-migrate-to-v2'
-}
+};
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com/';
 
