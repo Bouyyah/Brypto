d3a3bd5db5b93193a3bcf86bd544735f140e385d
Ayyoub Hamame <Hamameayyoub@gmail.com>
2023-06-15 12:39:16 +0100

    fix: API keys

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const newsApiHeaders = {
-    'x-bingapis-sdk': 'true',
-    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'x-rapidapi-key': '45a4b6d1fbmsh31b048dfc5744cbp19bb52jsn6d633361274c'
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+  'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
 }
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
